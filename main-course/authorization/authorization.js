4a2dd836f665978d51075a8ea65b8a94c293b6fb
AleksandrSherman <aleksandrserman@gmail.com>
2022-08-10 09:13:17 +0300

    Finish authorization task

--- main-course/authorization/authorization.js
+++ main-course/authorization/authorization.js
@@ -1,113 +1,176 @@
 import 'dotenv/config';
 
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import express from 'express';
-import sha256 from 'crypto-js/sha256.js';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const port = 3000;
 const app = express();
 app.use(express.json());
 
+let usersCollection;
+
 const SECRET_KEY = process.env.SECRET_KEY;
 
-const url = 'mongodb://localhost:27017/';
-
-function randomTokenTime(max, min){
+function randomTokenTime(max, min) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+async function getUserByEmail(email) {
+    const user = await usersCollection.findOne({ email });
+    return user === null ? undefined : user
+}
 
-async function registrationInDataBase(userInformation) {
-    const client = await MongoClient.connect(url);
+async function getUserById(id) {
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+    return user === null ? undefined : user
+}
 
-    const db = client.db('authorization-data-base');
-    const coll = db.collection('emails-passwords');
+async function createUser(userInformation) {
+    await usersCollection.insertOne(userInformation);
+}
 
+function generateJwtTokens(id) {
+    const accessToken = jwt.sign({ _id: id, type: 'access' }, SECRET_KEY, { expiresIn: randomTokenTime(60, 30) });
+    const refreshToken = jwt.sign({ _id: id, type: 'refresh' }, SECRET_KEY, { expiresIn: '30d' });
+    return { accessToken, refreshToken }
+}
 
-    let response = {};
-    if (!(await coll.findOne({ email: userInformation.email }))) {
-        await coll.insertOne(userInformation);
-        response.status = 'Registred successfully';
-    } else {
-        response = { 'status': 'You are already signed up' }
+async function authMiddleware(authorizationHeader) {
+    if (typeof authorizationHeader === 'undefined') {
+        throw Error('Unauthorized');
     }
 
-    await client.close();
+    if (authorizationHeader.split(' ').length !== 2 || authorizationHeader.split(' ')[0] !== 'Bearer') {
+        throw Error('Unauthorized');
+    }
 
-    return response;
+    const accessToken = authorizationHeader.split(' ')[1];
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(accessToken, SECRET_KEY);
+    } catch (err) {
+        throw Error('Token is not valid');
+    }
+
+    if (decodedToken.type !== 'access') {
+        throw Error('Token is not valid');
+    }
+
+    const user = await getUserById(decodedToken._id);
+
+    if (typeof user === 'undefined') {
+        throw Error('User is not found');
+    }
+
+    return user;
 }
 
-async function authorizationInDataBase(userInformation) {
-    const client = await MongoClient.connect(url);
+app.post('/sign-up', async (req, res) => {
+    const { email, password } = req.body;
 
-    const db = client.db('authorization-data-base');
-    const coll = db.collection('emails-passwords');
-
-    const currentUser = await coll.findOne({ email: userInformation.email, password: userInformation.password });
-
-    let errorStatus;
-    if ( !(currentUser) ) {
-        errorStatus = { 'status': 'Incorrect email or password' };
-
-        await client.close();
-        return errorStatus;
-
-    } 
-    if ((typeof currentUser.token === 'undefined')) {
-
-        const token = {$set: { token: jwt.sign( { '_id': currentUser._id }, SECRET_KEY, { expiresIn: randomTokenTime(60, 30) } ) } }
-        await coll.updateOne(currentUser, token);
-
-    } 
-
-    await client.close();
-    return currentUser;
-}
-
-app.post('/sign_up', async (req, res) => {
-    const userRegistrationEmail = req.body.email;
-    const userRegistrationPassword = sha256(req.body.password).toString();
-
-    if (typeof userRegistrationEmail === 'undefined') {
+    if (typeof email === 'undefined') {
         return res.status(400).json({ error: 'Body should contain \'email\' parameter' })
     }
-    if (typeof userRegistrationPassword === 'undefined') {
+    if (typeof password === 'undefined') {
         return res.status(400).json({ error: 'Body should contain \'password\' parameter' })
     }
-
-    const userInformation = {
-        'email': userRegistrationEmail,
-        'password': userRegistrationPassword
+    if(email.length < 5 && password.length < 5){
+        return res.status(400).json({ error: '\'Email\' and \'password\' should be more than 4 symbols' })
     }
 
-    const responseRegistration = await registrationInDataBase(userInformation);
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
 
-    return res.status(200).json(responseRegistration);
+    const userInformation = { email, password: hash };
+
+    const user = await getUserByEmail(userInformation.email);
+
+    if ( typeof user !== 'undefined' ) {
+        return res.status(400).json({ error: 'Current email is already registered' })
+    }
+    await createUser(userInformation);
+
+    const tokens = generateJwtTokens(userInformation._id);
+
+    return res.status(200).json(tokens);
 })
 
 app.post('/login', async (req, res) => {
-    const userAuthorizationEmail = req.body.email;
-    const userAuthorizationPassword = sha256(req.body.password).toString();
+    const { email, password } = req.body;
 
-    if (typeof userAuthorizationEmail === 'undefined') {
-        return res.status(400).json({ error: 'Invalid \'email\' parameter' })
+    if (typeof email === 'undefined') {
+        return res.status(400).json({ error: 'Body should contain \'email\' parameter' });
     }
-    if (typeof userAuthorizationPassword === 'undefined') {
-        return res.status(400).json({ error: 'Invalid \'password\' parameter' })
-    }
-
-    const userInformation = {
-        'email': userAuthorizationEmail,
-        'password': userAuthorizationPassword
+    if (typeof password === 'undefined') {
+        return res.status(400).json({ error: 'Body should contain \'password\' parameter' });
     }
 
-    const responseAuthorization = await authorizationInDataBase(userInformation);
+    const user = await getUserByEmail(email);
 
-    return res.status(200).json(responseAuthorization);
+    if (typeof user === 'undefined') {
+        return res.status(400).json({ error: 'This password or email is invalid' });
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, user.password);
+
+    if (!isValidPassword) {
+        return res.status(400).json({ error: 'This password or email is invalid' });
+    }
+
+    const tokens = generateJwtTokens(user._id);
+
+    return res.status(200).json(tokens);
 })
 
-function main() {
+app.post('/refresh', async (req, res) => {
+    const authorizationHeader = req.headers.authorization;
+
+    if (typeof authorizationHeader === 'undefined') {
+        throw Error('Unauthorized');
+    }
+
+    if (authorizationHeader.split(' ').length !== 2 || authorizationHeader.split(' ')[0] !== 'Bearer') {
+        throw Error('Unauthorized');
+    }
+
+    const refreshToken = authorizationHeader.split(' ')[1];
+
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(refreshToken, SECRET_KEY);
+    } catch (err) {
+        throw Error('Token is not valid');
+    }
+
+    if (decodedToken.type !== 'refresh') {
+        throw Error('Token is not valid');
+    }
+
+    const tokens = generateJwtTokens(decodedToken._id);
+
+    return res.status(200).json(tokens);
+})
+
+app.get('/me[0-9]', async (req, res) => {
+
+    let user;
+    try {
+        user = await authMiddleware(req.headers.authorization);
+    } catch (err) {
+        return res.status(400).json({ error: err.message })
+    }
+
+    return res.status(200).json({ request_num: req.url[req.url.length - 1], data: { username: user.email } });
+})
+
+async function main() {
+    const client = await MongoClient.connect('mongodb://localhost:27017/');
+
+    const db = client.db('authorization-data-base');
+    usersCollection = db.collection('emails-passwords');
 
     app.listen(port, () => {
         console.log(`The server runs on port: ${port}`)
